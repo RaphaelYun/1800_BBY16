469785bdbea1189f876032975b882405272cb7ae
unknown <raphael.yun@gmail.com>
2021-11-06 17:18:17 -0700

    implemented firebase log in system

---
+++ scripts/firebaseAPI_BBY16.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDwujPR8F-r79YZZROGKuhzzKeOiRXjil8",
+  authDomain: "bby16-572cc.firebaseapp.com",
+  projectId: "bby16-572cc",
+  storageBucket: "bby16-572cc.appspot.com",
+  messagingSenderId: "189172108238",
+  appId: "1:189172108238:web:6300d9fb07c409d019b99f",
+  measurementId: "G-N6C1DL5Q2S"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
